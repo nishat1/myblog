b22f18e11e992357f44a9a5a5342a1644578ad68
nishat1 <nishatgupta1@gmail.com>
2018-11-13 22:38:05 -0800

    Routing foundation complete

---
+++ src/components/PasswordForget.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PasswordForgetPage = () => {
+    return (
+        <div>
+            <h1>Password Forget Page</h1>
+        </div>
+    );
+};
+
+export default PasswordForgetPage;
